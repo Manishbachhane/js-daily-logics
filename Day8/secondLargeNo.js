7952c8826702bc4ca314716abef038f449076140
Manishbachhane <manishbachhane@gmail.com>
2025-12-11 22:55:15 +0530

    Array

---
+++ Day8/secondLargeNo.js
@@ -0,0 +1,13 @@
+const arr = [5, 9, 1, 7, 6];
+
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let num of arr) {
+  if (num > largest) {
+    secondLargest = largest;
+    largest = num;
+  } else if (num > secondLargest && num !== largest) {
+    secondLargest = num;
+  }
+}
