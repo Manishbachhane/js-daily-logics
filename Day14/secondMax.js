e30ea1ceecad7221919528a44a61cff40903d334
Manishbachhane <manishbachhane@gmail.com>
2026-03-05 15:38:26 +0530

    in second max handle negetive number

--- Day14/secondMax.js
+++ Day14/secondMax.js
@@ -1,15 +1,16 @@
 //second max number  by single loop
 
-let arr=[23, 87, 41, 56, 92, 14, 78, 65, 33, 90, 27, 48, 71, 19, 60];
+let arr=[1123, 187, 41, 56, 92, 14, 78, 65, 33, 90, 27, 48, 71, 19, 189];
 
 function secondMax(arr1){
-  let max=arr[0],secondMax=arr[0];
-  for(let i=0;i<arr.length;i++){
-    if(max<arr[i]){
+  let max=-Infinity,secondMax=-Infinity;
+  for(let i=0;i<arr1.length;i++){
+    if(max<arr1[i]){
       secondMax=max;
-      max=arr[i];
-    }
-  }
+      max=arr1[i];
+    }else if(arr1[i]>secondMax && arr1[i]!=max){
+      secondMax=arr1[i];
+    }  }
 
 
   console.log(max,secondMax);
