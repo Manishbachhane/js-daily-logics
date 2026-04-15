739550fc0c01410ced7faf9962254eb378bb8eb0
Manishbachhane <manishbachhane@gmail.com>
2026-04-16 03:27:20 +0530

    day20'

---
+++ Day19/oddNumberPattern.js
@@ -0,0 +1,14 @@
+function fun(){
+  let num=1;
+  for(let i=1;i<=5;i++){
+    let row="";
+    for(let j=1;j<=i;j++){
+      row+=num+" ";
+      num+=2;
+    }
+    console.log(row);
+    row="";
+  }
+}
+
+fun();
---
+++ Day20/pattern.js
@@ -0,0 +1,18 @@
+function fun(){
+
+
+    for(let i=1;i<=5;i++){
+      let row="";
+      for(let j=1;j<=5;j++){
+        if(i==j||i<=j){
+          row+="*";
+        }else{
+          row+=" ";
+        }
+      }
+      console.log(row);
+    }
+
+}
+
+fun();
